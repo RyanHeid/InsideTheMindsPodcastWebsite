6865a0d3792773a56bfc3a4f6664c4e99d3844d7
RyanHeid <ryan.jacob.heid@gmail.com>
2022-12-22 19:05:13 -0800

    Added alt image names.

--- mail.php
+++ mail.php
@@ -1,31 +1,13 @@
 <?php
- require_once "Mail.php";
- 
- $from = "Sandra Sender <sender@example.com>";
- $to = "Ramona Recipient <ryanheid@insidethemindspodcast.com>";
- $subject = "Hi!";
- $body = "Hi,
+$email = $_POST['email']; // this is the email address of the person filling the contact form
+$name = $_POST['name']; // name of the sender
+$whom = $_POST['whom']; // email address where the contact form data will be sent
+$subject = "Email from Contact form";
 
-How are you?";
- 
- $host = "mail.insidethemindspodcast.com";
- $username = "smtp_username";
- $password = "smtp_password";
- 
- $headers = array ('From' => $from,
-   'To' => $to,
-   'Subject' => $subject);
- $smtp = Mail::factory('smtp',
-   array ('host' => $host,
-     'auth' => true,
-     'username' => $username,
-     'password' => $password));
- 
- $mail = $smtp->send($to, $headers, $body);
- 
- if (PEAR::isError($mail)) {
-   echo("<p>" . $mail->getMessage() . "</p>");
-  } else {
-   echo("<p>Message successfully sent!</p>");
-  }
+$comment = $name . " " . $email . " wrote the following:" . "\n\n" . $_POST['comment'];
+
+mail($whom,$subject,$comment); // this is the email function
+
+echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+
  ?>
